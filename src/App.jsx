dbf0912db9c8b2f89fc9b8dc16a9db96102e1718
unknown <vassetethan@gmail.com>
2025-05-10 00:52:41 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Routes, Route, NavLink } from 'react-router-dom';
 import About from './components/About';
 import Discography from './components/Discography';
@@ -7,16 +8,28 @@ import Shakira360 from './components/Shakira360';
 import DiscordCommunity from './components/DiscordCommunity';
 
 export default function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <>
       <nav className="navbar">
-        <NavLink to="/" end>À propos</NavLink>
-        <NavLink to="/discography">Discographie</NavLink>
-        <NavLink to="/map">Carte</NavLink>
-        <NavLink to="/timeline">Timeline</NavLink>
-        <NavLink to="/shakira360">Anecdotes, influence et citations</NavLink>
-        <NavLink to="/discord">Notre Discord</NavLink>
+        <div className="navbar-logo">🌟 Shakira</div>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+        <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+          <NavLink to="/" end onClick={closeMenu}>À propos</NavLink>
+          <NavLink to="/discography" onClick={closeMenu}>Discographie</NavLink>
+          <NavLink to="/map" onClick={closeMenu}>Carte</NavLink>
+          <NavLink to="/timeline" onClick={closeMenu}>Timeline</NavLink>
+          <NavLink to="/shakira360" onClick={closeMenu}>Anecdotes</NavLink>
+          <NavLink to="/discord" onClick={closeMenu}>Discord</NavLink>
+        </div>
       </nav>
+
       <main>
         <Routes>
           <Route path="/" element={<About />} />
