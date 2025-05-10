7e94a12b9fb32f18a134c97e74e61dc1bfcc27fc
unknown <vassetethan@gmail.com>
2025-05-10 14:56:14 +0200

    Updates

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer style={{ marginTop: '3rem', textAlign: 'center', padding: '2rem', background: '#f0f0f0' }}>
-      <p>© 2025 Site hommage à Shakira | Créé avec ❤️</p>
-    </footer>
-  );
-}
